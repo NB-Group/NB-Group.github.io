51b6320d58644bdd5f8c2514897045e06513df0c
User782Tec <115236204+User782Tec@users.noreply.github.com>
2025-02-14 20:06:56 +0800

    介绍页面 v0.2.1

--- nb-music/vite.config.js
+++ nb-music/vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+
+function iframeSrcPlugin() {
+  return {
+    name: 'iframe-src-plugin',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<iframe\s+[^>]*src="([^"]+)"[^>]*><\/iframe>/g,
+        (match, src) => {
+          // 在这里处理 iframe 的 src 属性，例如将其替换为 Vite 的资源路径
+          const newSrc = `.${src}`;
+          return match.replace(src, newSrc);
+        }
+      );
+    },
+  };
+}
 
 export default defineConfig({
-    base: "./"
-    // other configurations
+  plugins: [iframeSrcPlugin()],
+  base: "./"
 });
