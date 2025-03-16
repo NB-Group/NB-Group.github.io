01c7c37c9683d5eef6c062ae06bc51749a4496fa
NB_Group <nb_group@qq.com>
2025-03-16 13:38:47 +0800

    修bug

--- nb-music/scripts/main.js
+++ nb-music/scripts/main.js
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // 平滑滚动
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
-            e.preventDefault();
             const targetId = this.getAttribute('href');
             if (targetId === '#') return;
             
